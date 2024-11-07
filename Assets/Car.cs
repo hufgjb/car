33eb7115cf3d7ecba36b659ce0728c96469fbe2b
huf <hufgjb226@gmail.com>
2024-11-07 12:17:22 +0800

    2024.11.07*

---
+++ Assets/Car.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+    float speed = 0;
+    Vector2 startPos;
+
+    void Start() {
+    }
+
+    void Update() {
+        if(Input.GetMouseButtonDown(0)) {       //點擊滑鼠時
+            this.speed = 0.2f;                  //設定初始速度
+        }
+
+        transform.Translate(this.speed, 0, 0);  //移動
+        this.speed *= 0.98f;                    //減速
+    }
+}
