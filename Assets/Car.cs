de0837133fdc6ddd4b7c1bc08741255abc70b53b
huf <hufgjb226@gmail.com>
2024-11-08 11:01:46 +0800

    2024.11.08

--- Assets/Car.cs
+++ Assets/Car.cs
@@ -11,9 +11,17 @@ public class CarController : MonoBehaviour {
     }
 
     void Update() {
-        if(Input.GetMouseButtonDown(0)) {       //點擊滑鼠時
-            this.speed = 0.2f;                  //設定初始速度
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPos = Input.mousePosition; //將點擊滑鼠時的座標記錄起來
         }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition; //將放開滑鼠時的座標記錄起來
+            float swipeLength = endPos.x - startPos.x;
+            speed = swipeLength / 500.0f; //把滑動長度轉換成初始移動速度
+        }
+
 
         transform.Translate(this.speed, 0, 0);  //移動
         this.speed *= 0.98f;                    //減速
